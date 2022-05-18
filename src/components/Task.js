b407289392b4f4d222b7f7a94d840b8905d86bd3
salissadiq <salissadiq@gmail.com>
2022-05-18 13:50:43 +0100

    Add reminder feature

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import {FaTimes} from 'react-icons/fa';
 export default function Task({task, onDelete, onReminder}) {
   return (
-    <div className="task" onDoubleClick={()=>onReminder(task.id)} >
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onReminder(task.id)} >
         <h3> {task.text} <FaTimes style={{color: 'red'}} onClick={()=>onDelete(task)} /> </h3>
         <p> {task.day} </p>
     </div>
